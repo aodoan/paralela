8a526d3343bed83a81cacf62dba00182b15d6e1b
Claudinei Alduan <caaf20@inf.ufpr.br>
2023-09-13 18:26:04 -0300

    sim

---
+++ t1/func.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define OK 1
+#define NOK 200 
+
+int verifyOutput( Input, Output, nTotalElements, k ){ 
+    
+
+
+    return NOK;
+
+    return OK;
+
+}
--- t1/heap.c
+++ t1/heap.c
@@ -2,8 +2,31 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+#define n 10
 int nTotalElements, k, nThreads;
 float *Input;
+
+#define OK 2012
+#define NOK 2000 
+
+int cmpfunc (const void * a, const void * b)
+{
+  float fa = *(const float*) a;
+  float fb = *(const float*) b;
+  return (fa > fb) - (fa < fb);
+}
+
+int verifyOutput( Input, Output, nTotalElements, k ){ 
+    qsort(Input, n, sizeof(float), cmpfunc);
+
+    for(int i = 0; i < nTotalElements; i++){
+        if(Input != Output) return NOK;
+    }
+
+    return OK;
+
+}
+
 
 int main(int argc, char **argv){
     //get parameters from command line
@@ -14,11 +37,16 @@ int main(int argc, char **argv){
     nTotalElements = atoi(argv[1]);
     k = atoi(argv[2]);
     nThreads = atoi(argv[3]);
-
-    printf("nTotal %i \nk %i \nnThreads %i\n", nTotalElements, k, nThreads);
-
+    int a, b;
+    float v;
+    Input = malloc(sizeof(float) * n);
+    for(int i = 0; i < n; i++){
+        a = rand();
+        b = rand();
+        v = a * 1.0 + b;
+        Input[i] = v;
+    }    
     
-
 
     return 0;
 }
